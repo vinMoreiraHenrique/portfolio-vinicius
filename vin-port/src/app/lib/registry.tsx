8645fdf502390156049a27bfc386be7c4f4e77d3
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-05-12 16:24:28 -0300

    feat: theme sheet partially made, only colors and fonts

---
+++ vin-port/src/app/lib/registry.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import {
+  ServerStyleSheet,
+  StyleSheetManager,
+  ThemeProvider,
+} from "styled-components";
+import { theme } from "../styles/theme";
+import { GlobalStyles } from "../styles/globals";
+
+export default function StyledComponentsRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+      </ThemeProvider>
+    </StyleSheetManager>
+  );
+}
---
+++ vin-port/src/components/Header/style.ts
@@ -0,0 +1,14 @@
+"use client";
+import { theme } from "@/app/styles/theme";
+import { styled } from "styled-components";
+
+export const StyledHeader = styled.header`
+  background-color: ${theme.colors.primary};
+  width: 100%;
+  color: white;
+`;
+
+export const StyledContainer = styled.div`
+  background-color: pink;
+`;
+
